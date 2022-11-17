2e3ea6195aef4ffc591fb02342fc9d7251c724d2
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-18 01:48:33 +0600

    admin

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken') 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000
@@ -14,6 +14,24 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ckrddue.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res , next){
+    // console.log('paisi mama',req.headers.authorization);
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+        return res.status(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(error, decoded){
+        if(error){
+            return res.status(403).send({message: 'forbiden access'})
+        }
+        req.decoded = decoded
+        next()
+    })
+
+}
 
 async function run(){
     try{
@@ -86,8 +104,12 @@ async function run(){
         //     res.send(options)
         // })
 
-        app.get('/bookings', async(req, res) =>{
+        app.get('/bookings',verifyJWT, async(req, res) =>{
             const email = req.query.email
+            const decodedEmail = req.decoded.email
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'forbiden access'})
+            }
             const query = {
                 email: email
             }
@@ -125,13 +147,37 @@ async function run(){
             console.log(user);
             res.status(403).send({accessToken: ''})
         } )
-
+        app.get('/users', async(req, res)=>{
+            const query = {}
+            const users = await usersCollection.find(query).toArray()
+            res.send(users)
+        })
 
 
         app.post('/users', async(req, res) =>{
             const user = req.body
             console.log(user);
             const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+        app.put('/users/admin/:id',verifyJWT, async(req, res)=>{
+            const decodedEmail = req.decoded.email
+            const query = {
+                email: decodedEmail;
+            }
+            const user = await usersCollection.findOne(query)
+            if(user.role !== 'admin'){
+               return res.status(403).send({message: 'forbiden access'})
+            }
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true} 
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options)
             res.send(result)
         })
 
