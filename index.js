76dfd3441d03ac5a8441e0e0bdab77aa8bc3895e
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-19 02:57:44 +0600

    push

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ async function run(){
 
         const usersCollection = client.db('doctorsPortal').collection('users')
 
+        const doctorsCollection = client.db('doctorsPortal').collection('doctors')
+
 
         app.get('/appointmentOptions', async (req , res) =>{
             const date = req.query.date
@@ -190,6 +192,17 @@ async function run(){
                 }
             }
             const result = await usersCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+        app.get('/doctors', async(req, res)=>{
+            const query = {}
+            const doctors = await doctorsCollection.find(query).toArray()
+            res.send(doctors)
+        })
+
+        app.post('/doctors', async(req, res) =>{
+            const doctor = req.body;
+            const result = await doctorsCollection.insertOne(doctor)
             res.send(result)
         })
 
